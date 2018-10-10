d41fc2bddf36b3d4ab34a45f453ada485c149edf
Oussama Mlouk <o.mlouk@hotmail.co.uk>
2018-10-10 22:16:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,4 @@
 var playlist = {kanye: "stronger"}
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistname = "songTitle"
-  return playlist
+function updatePlaylist(obj, artistName, songTitle){
+  obj.artistname = "songTitle"
 }
